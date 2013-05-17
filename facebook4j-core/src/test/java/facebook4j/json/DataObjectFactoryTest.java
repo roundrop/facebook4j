763d04274b6d6488c56606d544d10294a1e52f77
roundrop <roundrop@gmail.com>
2013-05-18 01:29:19 +0900

    Test DataObjectFactory

---
+++ facebook4j-core/src/test/java/facebook4j/json/DataObjectFactoryTest.java
@@ -0,0 +1,11 @@
+package facebook4j.json;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ryu
+ * Date: 2013/05/18
+ * Time: 0:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataObjectFactoryTest {
+}
