738e455a64129424f9c0ff9e84bb4759e99d8ecc
Ryuji Yamashita <yamashita@atware.co.jp>
2017-03-30 15:19:09 +0900

    remove overwrote fields #106

--- facebook4j-core/src/main/java/facebook4j/PageVideoUpdate.java
+++ facebook4j-core/src/main/java/facebook4j/PageVideoUpdate.java
@@ -19,6 +19,7 @@ package facebook4j;
 import facebook4j.internal.http.HttpParameter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -27,89 +28,15 @@ import java.util.List;
  * @author Dimitry Kudryavtsev dk8996 at gmail.com
  */
 public class PageVideoUpdate extends VideoUpdate {
-
 	private static final long serialVersionUID = -7572933117144435735L;
 
-	private Media source;
-	private String title;
-	private String description;
-	private Boolean published;
-	private Integer scheduledPublishTime;
-	
     private FeedTargetingParameter feedTargeting;
     private TargetingParameter targeting;
     
 	public PageVideoUpdate(Media source) {
 		super(source);
-		this.source = source;
 	}
 
-	public Media getSource() {
-		return source;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public PageVideoUpdate title(String title) {
-		setTitle(title);
-		return this;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public PageVideoUpdate description(String description) {
-		setDescription(description);
-		return this;
-	}
-
-	public Boolean getPublished() {
-		return published;
-	}
-
-	public void setPublished(Boolean published) {
-		this.published = published;
-	}
-
-	public PageVideoUpdate published(boolean published) {
-		setPublished(published);
-		return this;
-	}
-
-	public Integer getScheduledPublishTime() {
-		return scheduledPublishTime;
-	}
-
-	public void setScheduledPublishTime(Integer scheduledPublishTime) {
-		this.scheduledPublishTime = scheduledPublishTime;
-	}
-
-	public void setScheduledPublishTime(Date scheduledPublishTime) {
-		long time = scheduledPublishTime.getTime() / 1000L;
-		setScheduledPublishTime(Long.valueOf(time).intValue());
-	}
-
-	public PageVideoUpdate scheduledPublishTime(Integer scheduledPublishTime) {
-		setScheduledPublishTime(scheduledPublishTime);
-		return this;
-	}
-
-	public PageVideoUpdate scheduledPublishTime(Date scheduledPublishTime) {
-		long time = scheduledPublishTime.getTime() / 1000L;
-		return scheduledPublishTime(Long.valueOf(time).intValue());
-	}
-	
     public TargetingParameter getTargeting() {
         return targeting;
     }
@@ -137,20 +64,7 @@ public class PageVideoUpdate extends VideoUpdate {
     }
 
 	/* package */HttpParameter[] asHttpParameterArray() {
-		List<HttpParameter> params = new ArrayList<HttpParameter>();
-		params.add(source.asHttpParameter("source"));
-		if (title != null) {
-			params.add(new HttpParameter("title", title));
-		}
-		if (description != null) {
-			params.add(new HttpParameter("description", description));
-		}
-		if (published != null) {
-			params.add(new HttpParameter("published", published));
-		}
-		if (scheduledPublishTime != null) {
-			params.add(new HttpParameter("scheduled_publish_time", scheduledPublishTime));
-		}
+		List<HttpParameter> params = new ArrayList<HttpParameter>(Arrays.asList(super.asHttpParameterArray()));
         if (targeting != null) {
             params.add(new HttpParameter("targeting", targeting.asJSONString()));
         }
@@ -162,53 +76,31 @@ public class PageVideoUpdate extends VideoUpdate {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (!(o instanceof PageVideoUpdate))
-			return false;
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		if (!super.equals(o)) return false;
 
 		PageVideoUpdate that = (PageVideoUpdate) o;
 
-		if (description != null ? !description.equals(that.description) : that.description != null)
+		if (feedTargeting != null ? !feedTargeting.equals(that.feedTargeting) : that.feedTargeting != null)
 			return false;
-		if (published != null ? !published.equals(that.published) : that.published != null)
-			return false;
-		if (scheduledPublishTime != null ? !scheduledPublishTime.equals(that.scheduledPublishTime)
-				: that.scheduledPublishTime != null)
-			return false;
-		if (source != null ? !source.equals(that.source) : that.source != null)
-			return false;
-		if (title != null ? !title.equals(that.title) : that.title != null)
-			return false;
-        if (feedTargeting != null ? !feedTargeting.equals(that.feedTargeting) : that.feedTargeting != null)
-            return false;
-        if (targeting != null ? !targeting.equals(that.targeting) : that.targeting != null) return false;
+		return targeting != null ? targeting.equals(that.targeting) : that.targeting == null;
 
-		return true;
 	}
 
 	@Override
 	public int hashCode() {
-		int result = source != null ? source.hashCode() : 0;
-		result = 31 * result + (title != null ? title.hashCode() : 0);
-		result = 31 * result + (description != null ? description.hashCode() : 0);
-		result = 31 * result + (published != null ? published.hashCode() : 0);
-		result = 31 * result + (scheduledPublishTime != null ? scheduledPublishTime.hashCode() : 0);
-        result = 31 * result + (targeting != null ? targeting.hashCode() : 0);
-        result = 31 * result + (feedTargeting != null ? feedTargeting.hashCode() : 0);
+		int result = super.hashCode();
+		result = 31 * result + (feedTargeting != null ? feedTargeting.hashCode() : 0);
+		result = 31 * result + (targeting != null ? targeting.hashCode() : 0);
 		return result;
 	}
 
 	@Override
-    public String toString() {
-        return "PageVideoUpdate{" +
-                "source=" + source +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", published=" + published +
-                ", scheduledPublishTime=" + scheduledPublishTime +
-                ", feedTargeting=" + feedTargeting +
-                ", published=" + published +
-                '}';
-    }
+	public String toString() {
+		return "PageVideoUpdate{" +
+				"feedTargeting=" + feedTargeting +
+				", targeting=" + targeting +
+				"} " + super.toString();
+	}
 }
