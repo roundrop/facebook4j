f122f50229dea1306454114fad31a63fba44caa6
Felipe Andrade <felipe.andrade0918@gmail.com>
2017-07-31 15:25:22 -0300

    version 2.4.11

--- facebook4j-core/src/main/java/facebook4j/Version.java
+++ facebook4j-core/src/main/java/facebook4j/Version.java
@@ -20,7 +20,7 @@ package facebook4j;
  * @author Ryuji Yamashita - roundrop at gmail.com
  */
 public final class Version {
-    private static final String VERSION = "2.4.10";
+    private static final String VERSION = "2.4.11";
     private static final String TITLE = "Facebook4J";
 
     private Version() {
