6b22cfa420e7bc2bf32fca2151cfbe6e6f34e25e
roundrop <roundrop@gmail.com>
2018-08-25 14:28:28 +0900

    investigate

--- facebook4j-core/src/test/java/facebook4j/JavaEnvTest.java
+++ facebook4j-core/src/test/java/facebook4j/JavaEnvTest.java
@@ -7,6 +7,8 @@ public class JavaEnvTest {
     @Test
     public void printEnv() {
         System.out.println("================================");
+        System.out.println(Runtime.class.getPackage().getImplementationVersion());
+        System.out.println("================================");
         System.out.println("java.version=" + System.getProperty("java.version"));
         System.out.println("java.specification.version=" + System.getProperty("java.specification.version"));
         System.out.println("java.vm.version=" + System.getProperty("java.vm.version"));
