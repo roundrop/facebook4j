7b64ebf3a80f5a3a7371dd9475d2c37df06fc3ad
roundrop <roundrop@gmail.com>
2019-01-04 12:01:19 +0900

    version 2.4.13

--- facebook4j-core/src/main/java/facebook4j/Version.java
+++ facebook4j-core/src/main/java/facebook4j/Version.java
@@ -20,7 +20,7 @@ package facebook4j;
  * @author Ryuji Yamashita - roundrop at gmail.com
  */
 public final class Version {
-    private static final String VERSION = "2.4.12";
+    private static final String VERSION = "2.4.13";
     private static final String TITLE = "Facebook4J";
 
     private Version() {
